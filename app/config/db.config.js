e5d7206abe14a9ca6108ae49ee2632eff28cc950
BigYossapon <bigyossapon.forwork@gmail.com>
2023-05-19 07:00:10 +0700

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,5 +3,5 @@ module.exports = {
   PORT: 27017,
   DB: 'projects',
   //url: "mongodb://localhost:27017/projects"
-  url: " mongodb+srv://admin:88888888@cluster0.1nco4pl.mongodb.net/"
+  url: ""
 };
